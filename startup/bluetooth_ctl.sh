60ae120ce7a42b20d0b001f19c0201b33388b36b
robotenique <julianogarcia_1997@hotmail.com>
2021-04-26 22:04:05 -0300

    update: sleep time in bluetooth script

--- startup/bluetooth_ctl.sh
+++ startup/bluetooth_ctl.sh
@@ -4,11 +4,11 @@
 BLUETOOTH_SPEAKER_MACADDR="7C:96:D2:63:11:A5"
 # unpair speaker
 echo -e "remove $BLUETOOTH_SPEAKER_MACADDR\n" | bluetoothctl
-# wait 5 seconds
-sleep 5
-# pair with blueooth speaker again
-echo -e "pair $BLUETOOTH_SPEAKER_MACADDR\n" | bluetoothctl
 # wait 2 seconds
 sleep 2
+# pair with blueooth speaker again
+echo -e "pair $BLUETOOTH_SPEAKER_MACADDR\n" | bluetoothctl
+# wait 5 seconds
+sleep 5
 # connect with blueooth speaker again
 echo -e "connect $BLUETOOTH_SPEAKER_MACADDR\n" | bluetoothctl
