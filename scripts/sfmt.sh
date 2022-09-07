6040682a87acd6ff79df7f6d662676e4e42bfb0b
juliano garcia <juliano.garcia@nubank.com.br>
2022-09-07 11:21:10 -0300

    update sfmt

--- scripts/sfmt.sh
+++ scripts/sfmt.sh
@@ -1,12 +1,28 @@
 ## This simple script will format a scala code that it's in the clipboard
 ## *only tested in MAC OS*
 rm -f _tempScript.scala
-# copy clipboard to temp file, inside current folder
-pbpaste > _tempScript.scala
+# matching keywords
+SUB="object "
+SUB2="case class "
+if [[ "$(pbpaste)" == *"$SUB"* ]] || [[ "$(pbpaste)" == *"$SUB2"* ]]; then
+    pbpaste >> _tempScript.scala
+else
+    echo "object A{" > _tempScript.scala
+    pbpaste >> _tempScript.scala
+    echo "\n}" >> _tempScript.scala
+fi
+
 # format the file w/ scalafmt; You need to install scalafmt: https://scalameta.org/scalafmt/docs/installation.html#cli
 # (you'll possibly have to install coursier and add it to the $PATH variable)
 echo "Formatting scala code..."
 scalafmt _tempScript.scala
-# copy back the formatted file to clipboard
-/bin/cat _tempScript.scala | pbcopy
+if [[ "$(pbpaste)" == *"$SUB"* ]] || [[ "$(pbpaste)" == *"$SUB2"* ]]; then
+    /bin/cat _tempScript.scala | pbcopy
+else
+    # copy back the formatted file to clipboard, removing the "object" wrapper
+    # remove the first two spaces of each line
+    # remove first line
+    # remove last line
+    sed "s/  //" _tempScript.scala | tail -n +2 | ghead -n -1 | pbcopy
+fi
 echo "Finished"
